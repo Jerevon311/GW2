c37f8170da02fee7547c1b208204c8fa8469951d
Jerevon <jerevon6311@126.com>
2018-12-05 21:17:43 +0800

    激战2九大职业更新

---
+++ GameCareer/src/main/java/com/Jerevon/GameCareer.java
@@ -0,0 +1,18 @@
+package com.Jerevon;
+
+/**
+ * Created by Jerevon on 2018/12/5.
+ */
+public class GameCareer {
+    public static void main(String[] args) {
+        System.out.println("Elementalist_元素使");
+        System.out.println("Necronmancer_死灵法师");
+        System.out.println("Mesmer_幻术师");
+        System.out.println("Thief_盗贼");
+        System.out.println("Engineer_工程师");
+        System.out.println("Ranger_游侠");
+        System.out.println("Warrior_战士");
+        System.out.println("Guardian_守护者");
+        System.out.println("Revenan_魂武者");
+    }
+}
